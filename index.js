bad5e8686dae73ad40dde119b3decfcb143712c7
IconicLuck <uhasib959@gmail.com>
2023-05-13 19:25:20 +0600

    implemented jwt verification for login

--- index.js
+++ index.js
@@ -21,6 +21,23 @@ const client = new MongoClient(uri, {
     }
 });
 
+const verifyJWT = (req, res, next) => {
+    // console.log(req.headers.authorization)
+
+    const authorization = req.headers.authorization
+    if(!authorization){
+        res.status(401).send({error: true, message: 'unauthorized access'})
+    }
+    const token = authorization.split(' ')[1]
+    jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (error, decoded) => {
+        if(error){
+            res.status(403).send({error: true, message: 'unauthorized access'})
+        }
+        req.decoded = decoded
+        next()
+    })
+}
+
 async function run() {
     try {
         // Connect the client to the server	(optional starting in v4.7)
@@ -61,8 +78,11 @@ async function run() {
 
         // Bookings
 
-        app.get('/bookings', async (req, res) => {
+        app.get('/bookings', verifyJWT, async (req, res) => {
             // console.log(req.query.email)
+
+            // console.log(req.headers.authorization)
+
             let query = {}
             if(req.query?.email){
                 query = {email: req.query.email}
